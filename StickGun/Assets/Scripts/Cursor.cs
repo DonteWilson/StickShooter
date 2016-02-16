80c0e71abd0ea08b9abfcc1744ff051c28d79871
dwilsonaieyr1 <JittNA@aol.com>
2016-02-16 09:31:00 -0600

    Copying files from old repo

---
+++ StickGun/Assets/Scripts/Cursor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+    
+   
+    
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {   
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x - 8, Input.mousePosition.y + 11, +10));
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, 10))
+        {
+            print("There is something in front of the object!");
+        }
+
+
+
+
+
+        }
+}
