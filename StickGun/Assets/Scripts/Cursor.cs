f332bd6527a7fb11642a73647afbb860a1536972
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-02-16 10:41:38 -0600

    Killing stick figures

--- StickGun/Assets/Scripts/Cursor.cs
+++ StickGun/Assets/Scripts/Cursor.cs
@@ -14,22 +14,24 @@ public class Cursor : MonoBehaviour {
 
     // Update is called once per frame
     void Update()
-    {   
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition, 10);
+
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
         transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x - 8, Input.mousePosition.y + 11, +10));
         if (Input.GetMouseButtonDown(0) == true)
         {
             GetComponent<AudioSource>().Play();
+
+            if (hit != null && hit.collider != null)
+            {
+                Destroy(hit.collider.gameObject);
+                //hit.collider.GetComponent<Health>().hp -= 1;
+                Debug.Log("hit");
+
+            }
         }
 
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        if (Physics.Raycast(transform.position, fwd, 10))
-        {
-            print("There is something in front of the object!");
-        }
 
-
-
-
-
-        }
+    }
 }
